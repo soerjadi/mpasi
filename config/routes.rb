5522119517a77475fd7a78fdff7160b297d0d1d4
surya <surya@degalab.com>
2017-03-17 10:42:28 +0700

    implement login with google oauth2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  resource :sessions, only: [:create, :destroy]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "application#main"
 end
